b143c8d4feb29ef7f0134be96daef24582e5c70f
ROMAN K <musosoft@gmail.com>
2020-01-31 02:03:00 +0100

    Revert CSS files, temporarily switch to NPM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LandingPage from './Views/LandingPage/LandingPage';
+import './assets/css/material-kit.css';
 import './assets/css/style.css';
+import './assets/css/google-fonts.css';
+import './assets/css/font-awesome.css';
 
 ReactDOM.render(<LandingPage />, document.getElementById('root'));
